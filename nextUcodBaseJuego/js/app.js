43cb2a9ebfbf5923df782f814d404d9c6ec91e60
Edwin Barahona <ed_barah@hotmail.com>
2021-01-21 16:39:42 -0600

    COMBO
    quitar repetidos del arreglo

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -16,6 +16,14 @@ function colorTituloAmarillo() {
     })
 }
 
+function destruirCombo(y, x) {
+
+
+    $("#" + y + "-" + x + "").toggle("puff")
+
+
+}
+
 colorTituloBlanco()
 
 //funcion numero aleatorio
@@ -35,22 +43,22 @@ var altoCelda = alto / 7;
 
 $(document).ready(function () {
 
-  
+
     //---------------------
 
     var rows = 7;
     var cols = 7;
     var grid = [];
-  
+
     for (var r = 0; r < rows; r++) {
         grid[r] = [];
-        $(".panel-tablero").append('<div class= "row-'+r+'" ></>')
+        $(".panel-tablero").append('<div class= "row-' + r + '" ></>')
 
         for (var c = 0; c < cols; c++) {
 
             grid[r][c] = numRandom()
 
-            $(".row-"+r+"").append('<div class="col-'+c+'"> <img src="image/' + grid[r][c] + '.png" class="candy" style="height:' + altoCelda + 'px; position: absolute"  id='+ grid[r][c] +' /></div>')
+            $(".row-" + r + "").append('<div class="col-' + c + '"> <img src="image/' + grid[r][c] + '.png" class="candy" style="height:' + altoCelda + 'px; position: absolute"  id=' + r + '-' + c + ' /></div>')
         }
     }
 
@@ -77,7 +85,7 @@ $(document).ready(function () {
         return {
             up: {
                 value: getCell(matrix, y - 1, x),
-                index: [y - 1 + "-" + x]
+                index: [(y - 1) + "-" + x]
             },
 
             right: {
@@ -93,8 +101,8 @@ $(document).ready(function () {
 
             left: {
                 value: getCell(matrix, y, x - 1),
-                index: [y + "-" + x - 1]
-
+                //index: [y + "-" + x - 1]
+                index: [y + '-' + (x - 1)]
             }
             // upRight:   getCell(matrix, y-1, x+1),
             //downRight: getCell(matrix, y+1, x+1),
@@ -126,82 +134,126 @@ $(document).ready(function () {
 
                 adyacentes = (surroundings(grid, y, x))
 
-                var up = adyacentes.up.value
+                var up = adyacentes[h].value
 
                 if (up == grid[y][x]) {
 
-                    let pcombo = (y + "-" + x + "/" + adyacentes.up.index)
-                    let nuevoAdyacente = adyacentes.up.index + ''
+                    let pcombo = (y + "-" + x + "/" + adyacentes[h].index)
+                    let nuevoAdyacente = adyacentes[h].index + ''
                     let adyacenteYX = nuevoAdyacente.split('-');
-
+                   
                     adyacentes = (surroundings(grid, adyacenteYX[0], adyacenteYX[1]))
 
-                    if (up == adyacentes.up.value) {
+                    if (up == adyacentes[h].value) {
+                        combo.push(pcombo + "/" + adyacentes[h].index)
 
-                        combo.push(pcombo + "/" + adyacentes.up.index)
-                       
+
                     }
-                   
+
                 }
-                
+
             }
-           
+
         }
+
         return combo;
     }
 
 
-    let comboV = combos("up");
-    let comboH = combos("left");
 
-    console.log(comboV)
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
+      sleep(1000).then(() => { 
+
+        let comboV = combos("up");
+        let comboH = combos("left");
+    
+        let comboVS = comboV + ''
+        let comboVS2 = (comboVS.split("/"))
+        let comboVS3 = (comboVS2 + '').split(",")
+    
+        let comboHS = comboH + ''
+        let comboHS2 = (comboHS.split("/"))
+        let comboHS3 = (comboHS2 + '').split(",")
+
+      console.log(comboVS3)
+    
+        for (var i = 0; i<comboVS3.length; i++){
+    
+            let comboArreglo = comboVS3[i].split("-")
+            
+            destruirCombo(comboArreglo[0], comboArreglo[1]) 
+            grid[comboArreglo[0]][comboArreglo[1]] = 0
+        }
+
+        for (var i = 0; i<comboHS3.length; i++){
+    
+            let comboArreglo = comboHS3[i].split("-")
+            
+            destruirCombo(comboArreglo[0], comboArreglo[1]) 
+            grid[comboArreglo[0]][comboArreglo[1]] = 0
+        }
+
+
+       
+
+       });
+
+
+
+    
+
+    //console.log(comboV)
     //console.log(comboH)
 
- 
-var box = $(".candy");
-var mainCanvas = $(".panel-tablero");
+
+    var box = $(".candy");
+    var mainCanvas = $(".panel-tablero");
 
 
-box.draggable({
-    containment: mainCanvas,
-    helper: "clone",
+    box.draggable({
+        containment: mainCanvas,
+        helper: "clone",
 
-    start: function () {
-       $(this).css({
-            opacity: 0
-        });
+        start: function () {
+            $(this).css({
+                opacity: 0
+            });
 
-       $(".candy").css("z-index", "0");
-    },
+            $(".candy").css("z-index", "0");
+        },
 
-    stop: function () {
-        $(this).css({
-            opacity: 3
-        });
-    }
-});
+        stop: function () {
+            $(this).css({
+                opacity: 3
+            });
+        }
+    });
 
-box.droppable({
-    accept: box,
+    box.droppable({
+        accept: box,
 
-    drop: function (event, ui) {
-        var draggable = ui.draggable;
-        var droppable = $(this);
-        var dragPos = draggable.position();
-        var dropPos = droppable.position();
+        drop: function (event, ui) {
+            var draggable = ui.draggable;
+            var droppable = $(this);
+            var dragPos = draggable.position();
+            var dropPos = droppable.position();
 
-        draggable.css({
-            left: dropPos.left + "px",
-            top: dropPos.top + "px",
-            "z-index": 20
-        });
+            draggable.css({
+                left: dropPos.left + "px",
+                top: dropPos.top + "px",
+                "z-index": 20
+            });
 
-        droppable.css("z-index", 10).animate({
-            left: dragPos.left,
-            top: dragPos.top
-        });
-    }
-});
+            droppable.css("z-index", 10).animate({
+                left: dragPos.left,
+                top: dragPos.top
+            });
+        }
+    });
 
 
 
